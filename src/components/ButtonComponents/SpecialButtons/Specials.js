cb0de20f2c8e0ade69b364fd5602a992f0b4202f
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-13 17:00:07 -0400

    finished specials js

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -13,6 +13,7 @@ const Specials = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {specialState.map(item => <SpecialButton value={item} key={item} />)}
     </div>
   );
 };
