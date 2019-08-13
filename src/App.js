15e51c99a1ce01f6815ec905a5e0e885416b2fbf
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-13 18:24:41 -0400

    added larger zero button

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
   const [memory, setMemory] = useState("0");
-
+  let holder = "";
   const changeMemory = (item) => {
     if (memory === "0") {
       setMemory(item);
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -4,7 +4,7 @@ const NumberButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button onClick={() => { props.changeMemory(props.value) }}>{props.value}</button>
+      <button className={"button-" + props.value} onClick={() => { props.changeMemory(props.value) }}>{props.value}</button>
     </>
   );
 };
