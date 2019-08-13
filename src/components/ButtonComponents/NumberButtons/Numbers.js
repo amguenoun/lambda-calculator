6a7abd493ee057a8cb99478fab46f35555bff896
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-13 16:27:37 -0400

    imported the data into wrapper components

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -3,9 +3,10 @@ import NumberButton from "./NumberButton";
 //import any components needed
 
 //Import your array data to from the provided data file
-
+import { numbers } from "../../../data";
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -3,7 +3,7 @@ import OperatorButton from "./OperatorButton";
 //import any components needed
 
 //Import your array data to from the provided data file
-
+import { operators } from "../../../data";
 const Operators = () => {
   // STEP 2 - add the imported data to state
   return (
