2feadde7d7e7fc79036c2a36bee2342c81ead224
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-13 16:56:58 -0400

    finished operator js

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -13,6 +13,7 @@ const Operators = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {operatorState.map(item => (<OperatorButton char={item.char} value={item.value} key={item.char} />))}
     </div>
   );
 };
