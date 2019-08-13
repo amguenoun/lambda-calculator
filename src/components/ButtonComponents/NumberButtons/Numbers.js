f0bb1fad79e7d13817f982215d4dc7c4282663b0
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-13 16:19:09 -0400

    set up imports and exports

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import NumberButton from "./NumberButton";
 //import any components needed
 
 //Import your array data to from the provided data file
@@ -14,3 +14,5 @@ const Numbers = () => {
     </div>
   );
 };
+
+export default Numbers;
